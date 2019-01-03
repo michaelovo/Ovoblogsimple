90194daaf74f24acfae5e4a77fe22544cb371014
michaelovo <emikeovo@gmail.com>
2019-01-03 06:45:25 -0800

    added migration and model 'blogpage' for pagecreator file

---
+++ database/migrations/2019_01_03_143220_create_blogpages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBlogpagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogpages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('pagename');
+            $table->string('blog_title');
+            $table->string('category');
+            $table->string('author');
+            $table->string('page_content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blogpages');
+    }
+}
