9911c0fdea373afee53ad06c25773131f20f6b56
michaelovo <emikeovo@gmail.com>
2019-01-09 02:36:51 -0800

    modified blogfile and adminlayout file

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -14,8 +14,9 @@
                     <div class="date meta-last">20 May | 2016</div>
                     <div class="category"><a href="#">Business</a></div>
                   </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <h3 class="h4">10 common fruits that are underrated</h3></a>
+                  <p class="text-muted">Fruit and vegetables are a good source of vitamins and minerals, including folate,
+                    vitamin C and potassium. </p>
                   <footer class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
                       <div class="avatar"><img src="img/avatar-3.jpg" alt="..." class="img-fluid"></div>
                       <div class="title"><span>John Doe</span></div></a>
@@ -29,11 +30,11 @@
                 <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-2.jpg" alt="..." class="img-fluid"></a></div>
                 <div class="post-details">
                   <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
+                    <div class="date meta-last">09 January | 2019</div>
                     <div class="category"><a href="#">Business</a></div>
                   </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <h3 class="h4">Why you should never give up</h3></a>
+                  <p class="text-muted">It is a known fact that 80 percent of New Year's resolutions fail, but this year it's time to leave the failing to others.</p>
                   <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
                       <div class="avatar"><img src="img/avatar-2.jpg" alt="..." class="img-fluid"></div>
                       <div class="title"><span>John Doe</span></div></a>
@@ -47,11 +48,11 @@
                 <div class="post-thumbnail"><a href="post.html"><img src="img/blog-post-3.jpeg" alt="..." class="img-fluid"></a></div>
                 <div class="post-details">
                   <div class="post-meta d-flex justify-content-between">
-                    <div class="date meta-last">20 May | 2016</div>
+                    <div class="date meta-last">01 January | 2019</div>
                     <div class="category"><a href="#">Business</a></div>
                   </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <h3 class="h4">Why 2019 is special</h3></a>
+                  <p class="text-muted">2019 is a great year to make money, and a good year to invest!</p>
                   <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
                       <div class="avatar"><img src="img/avatar-3.jpg" alt="..." class="img-fluid"></div>
                       <div class="title"><span>John Doe</span></div></a>
@@ -66,10 +67,11 @@
                 <div class="post-details">
                   <div class="post-meta d-flex justify-content-between">
                     <div class="date meta-last">20 May | 2016</div>
-                    <div class="category"><a href="#">Business</a></div>
+                    <div class="category"><a href="#">TECHNOLOGY</a></div>
                   </div><a href="post.html">
-                    <h3 class="h4">Alberto Savoia Can Teach You About Interior</h3></a>
-                  <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>
+                    <h3 class="h4">Why every programmer should use Laravel</h3></a>
+                  <p class="text-muted">Laravel is an open-source PHP framework, which is robust and easy to understand.
+                  </p>
                   <div class="post-footer d-flex align-items-center"><a href="#" class="author d-flex align-items-center flex-wrap">
                       <div class="avatar"><img src="img/avatar-1.jpg" alt="..." class="img-fluid"></div>
                       <div class="title"><span>John Doe</span></div></a>
@@ -91,5 +93,5 @@
             </nav>
           </div>
         </main>
-    
+
 @endsection
